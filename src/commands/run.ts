80d939d8d7eccc5eb6bc4a2641b933a1d67234ed
Francesco Virga <francescod.virga@gmail.com>
2020-09-24 17:13:17 -0400

    Fix default path

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -23,7 +23,8 @@ export default class Run extends Command {
     static args = [
         {
             name: 'path',
-            default: path.join(process.cwd(), "src/models"),
+            // default: path.join(process.cwd(), "./src/models"),
+            default: "./src/models",
         },
     ]
 
