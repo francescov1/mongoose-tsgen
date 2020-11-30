d980a16f5e22f79352a0cb574b574211c85c4889
Francesco Virga <francescod.virga@gmail.com>
2020-11-30 10:04:04 -0500

    Remove parsed path comments

--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -25,29 +25,6 @@ export const getConfigFromFile = (configPath?: string): object => {
   return JSON.parse(rawConfig);
 };
 
-// interface ParsedPath {
-//   /**
-//    * The root of the path such as '/' or 'c:\'
-//    */
-//   root: string;
-//   /**
-//    * The full directory path such as '/home/user/dir' or 'c:\path\dir'
-//    */
-//   dir: string;
-//   /**
-//    * The file name including extension (if any) such as 'index.html'
-//    */
-//   base: string;
-//   /**
-//    * The file extension (if any) such as '.html'
-//    */
-//   ext: string;
-//   /**
-//    * The file name without extension (if any) such as 'index'
-//    */
-//   name: string;
-// }
-
 export const getModelsPaths = (basePath: string, extension: "js" | "ts"): string[] => {
   const { base: basePathEnd } = path.parse(basePath);
   const searchPath =
