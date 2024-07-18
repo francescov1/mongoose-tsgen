01c72eb4d3237c7e62a271e65ec504c220a7ee10
Francesco Virga <francescod.virga@gmail.com>
2024-07-18 01:10:26 -0700

    Add base url to parseTSConfig

--- src/helpers/tests/tsReader.test.ts
+++ src/helpers/tests/tsReader.test.ts
@@ -1,14 +1,18 @@
 import { parseTSConfig } from "../tsReader";
 
 describe("parseTSConfig", () => {
-  test("path aliases should be resolved from extended tsconfig.json", () => {
+  test("path aliases and baseUrl should be resolved from extended tsconfig.json", () => {
     const testTsConfigPath = "tsconfig.test.json";
 
     const tsConfig = parseTSConfig(testTsConfigPath);
 
     // Check of extended path is present
     expect(tsConfig.compilerOptions.paths["@package-lock-json"][0]).toBe("./package-lock.json");
+    
     // Check if extended path is present and overridden
     expect(tsConfig.compilerOptions.paths["@package-json"][0]).toBe("./package.json");
+
+    // Ensure the parent's base URL is overridden by the child's
+    expect(tsConfig.compilerOptions.baseUrl).toBe("./a-base-url-from-child");
   });
 });
